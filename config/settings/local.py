9f497c83cc631bc1baf8481e27ec444bef50ff6a
kuzentio <0585ec@gmail.com>
2018-05-11 15:55:46 +0300

    add Logo icon

--- config/settings/local.py
+++ config/settings/local.py
@@ -22,6 +22,19 @@ INSTALLED_APPS += (
     # 'django_pdb',
 )
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework_datatables.renderers.DatatablesRenderer',
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'rest_framework_datatables.filters.DatatablesFilterBackend',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework_datatables.pagination.DatatablesPageNumberPagination',
+    'PAGE_SIZE': 50,
+}
+
 # MIDDLEWARE += (
 #     'django_pdb.middleware.PdbMiddleware',
 # )
