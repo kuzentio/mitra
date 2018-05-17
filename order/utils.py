30fbd5a4811ee506ebf92cc23f7f2bffcbf5d21a
kuzentio <0585ec@gmail.com>
2018-05-17 23:25:51 +0300

    Result header table for orders

--- order/utils.py
+++ order/utils.py
@@ -1,24 +1,58 @@
-from django.db.models import Avg
+from decimal import Decimal
+from django.db.models import Sum, F, Avg
 
 from order import constance
+
+
+def get_amount_from_avg_order(queryset, _type, _quantity):
+    aggregation = queryset.all().filter(
+        type=_type
+    ).aggregate(
+        Avg('price'),
+    ) or Decimal('0.0')
+    result = Decimal(aggregation['price__avg']) * _quantity
+
+    return result
+
+
+def get_not_matched_quantity(queryset):
+    sell = queryset.filter(
+        type=constance.ORDER_TYPE_SELL
+    ).aggregate(
+        Sum('quantity')
+    )
+    buy = queryset.filter(
+        type=constance.ORDER_TYPE_BUY
+    ).aggregate(
+        Sum('quantity')
+    )
+    buy['quantity__sum'] = buy['quantity__sum'] or Decimal('0.0')
+    sell['quantity__sum'] = sell['quantity__sum'] or Decimal('0.0')
+    _type = constance.ORDER_TYPE_BUY if buy['quantity__sum'] >= sell['quantity__sum'] else constance.ORDER_TYPE_SELL
+
+    return _type, buy['quantity__sum'] - sell['quantity__sum']
 
 
 def aggregate_orders_by_types(queryset):
     aggregations = {}
 
     for any_type in [constance.ORDER_TYPE_SELL, constance.ORDER_TYPE_BUY]:
-        if queryset.filter(type=any_type).exists():
-            aggregations[any_type] = queryset.filter(
-                type=any_type
-            ).aggregate(
-                Avg('price'),
-                Avg('quantity'),
-                Avg('commission')
-            )
-            price = aggregations[any_type]['price__avg']
-            quantity = aggregations[any_type]['quantity__avg']
-            commission = aggregations[any_type]['commission__avg']
-            avg_pull_per_order = (price * quantity) - commission
-            aggregations[any_type]['avg_pull_per_order'] = avg_pull_per_order
+        aggregations[any_type] = queryset.filter(
+            type=any_type
+        ).annotate(
+            total=(F('quantity') * F('price')),
+        ).aggregate(
+            Sum('total'),
+        )
+    _type, _quantity = get_not_matched_quantity(queryset.all())
+    if _quantity:
+        aggregations[_type]['total__sum'] = aggregations[_type]['total__sum'] - get_amount_from_avg_order(queryset, _type, _quantity)
+
+    total_buy = aggregations[constance.ORDER_TYPE_BUY].pop('total__sum') or Decimal('0.0')
+    total_sell = aggregations[constance.ORDER_TYPE_SELL].pop('total__sum') or Decimal('0.0')
+
+    aggregations[constance.ORDER_TYPE_BUY] = total_buy
+    aggregations[constance.ORDER_TYPE_SELL] = total_sell
+    aggregations['revenue'] = total_sell - total_buy
 
     return aggregations
