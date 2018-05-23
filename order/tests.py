a6687634a1fb1baf081f7d69fa85965b46459b55
kuzentio <0585ec@gmail.com>
2018-05-24 01:03:41 +0300

    add tests

--- order/tests.py
+++ order/tests.py
@@ -1,41 +1,90 @@
-import decimal
-import random
-
-from django.db.models import Avg
+from decimal import Decimal
 from django.test import TestCase
 
+from order.constance import ORDER_TYPE_BUY, ORDER_TYPE_SELL
 from order.factories import OrderFactory
 from order.models import Order
-from order.utils import aggregate_orders_by_types
+from order.utils import aggregate_orders_by_types, get_amount_from_avg_order
 from profile.factories import AccountFactory
 
 
 class TestOrderCreation(TestCase):
-    def test_aggregation_orders_by_types_success(self):
-        account = AccountFactory()
-        for i in range(1, 20):
-            OrderFactory(
-                account=account,
+    def setUp(self):
+        self.account = AccountFactory()
+        self.order_quantity = Decimal('1.0')
+        self.order_commission = Decimal('0.5')
+        self.order_price = Decimal('10.0')
+
+    def test_utils_aggregation_orders_by_types(self):
+        for i in range(0, 6):
+            OrderFactory.create(
+                account=self.account,
                 pair='BTC-MANA',
-                quantity=decimal.Decimal(random.randrange(10, 35))/100,
-                commission=decimal.Decimal(random.randrange(1, 5))/1000,
-                price=decimal.Decimal(random.randrange(10, 50))/1000,
+                quantity=self.order_quantity,
+                commission=self.order_commission,
+                price=self.order_price,
             )
         payload = aggregate_orders_by_types(Order.objects.all())
+        payload.pop('revenue')
+        self.assertDictEqual(
+            {
+                'pairs': ['BTC-MANA'],
+                'BUY': ((self.order_quantity * self.order_price) + self.order_commission) * 3,
+                'SELL': ((self.order_quantity* self.order_price) - self.order_commission) * 3,
+            }, payload
+        )
 
-        expected_data = {
-            "BUY": Order.objects.filter(type='BUY').aggregate(
-                Avg('price'),
-                Avg('quantity'),
-                Avg('commission')
-            ),
-            "SELL": Order.objects.filter(type='SELL').aggregate(
-                Avg('price'),
-                Avg('quantity'),
-                Avg('commission')
+    def test_utils_aggregation_orders_with_not_matched_orders(self):
+        for i in range(0, 7):
+            OrderFactory.create(
+                account=self.account,
+                pair='BTC-MANA',
+                quantity=self.order_quantity,
+                commission=self.order_commission,
+                price=self.order_price,
             )
-        }
+        payload = aggregate_orders_by_types(Order.objects.all())
+        payload.pop('revenue')
 
+        self.assertDictEqual(
+            {
+                'pairs': ['BTC-MANA'],
+                'BUY': ((self.order_quantity * self.order_price) + self.order_commission) * 3,
+                'SELL': ((self.order_quantity * self.order_price) - self.order_commission) * 3,
+            }, payload
+        )
 
+    def test_utils_get_amount_from_avg_order_for_type_sell(self):
+        for i in range(0, 7):
+            OrderFactory.create(
+                account=self.account,
+                pair='BTC-MANA',
+                quantity=self.order_quantity,
+                commission=self.order_commission,
+                price=self.order_price,
+            )
 
+        amount = get_amount_from_avg_order(Order.objects.all(), ORDER_TYPE_SELL, self.order_quantity)
+        self.assertEqual(amount, (self.order_price * self.order_quantity) - self.order_commission)
 
+    def test_utils_get_amount_from_avg_order_for_type_buy(self):
+        for i in range(0, 2):
+            OrderFactory.create(
+                account=self.account,
+                pair='BTC-MANA',
+                type=ORDER_TYPE_SELL,
+                quantity=self.order_quantity,
+                commission=self.order_commission,
+                price=self.order_price,
+            )
+        for i in range(0, 4):
+            OrderFactory.create(
+                account=self.account,
+                pair='BTC-MANA',
+                type=ORDER_TYPE_BUY,
+                quantity=self.order_quantity,
+                commission=self.order_commission,
+                price=self.order_price,
+            )
+        amount = get_amount_from_avg_order(Order.objects.all(), ORDER_TYPE_SELL, self.order_quantity)
+        self.assertEqual(amount, (self.order_price * self.order_quantity) - self.order_commission)
