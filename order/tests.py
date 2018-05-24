a25e5829d3d9b798d8ff715cbd5baf5a29d3710b
kuzentio <0585ec@gmail.com>
2018-05-24 15:11:45 +0300

    add tests

--- order/tests.py
+++ order/tests.py
@@ -4,18 +4,18 @@ from django.test import TestCase
 from order.constance import ORDER_TYPE_BUY, ORDER_TYPE_SELL
 from order.factories import OrderFactory
 from order.models import Order
-from order.utils import aggregate_orders_by_types, get_amount_from_avg_order
+from order.utils import aggregate_orders_by_types, get_amount_from_avg_order, get_not_matched_quantity
 from profile.factories import AccountFactory
 
 
-class TestOrderCreation(TestCase):
+class TestOrderUtils(TestCase):
     def setUp(self):
         self.account = AccountFactory()
         self.order_quantity = Decimal('1.0')
         self.order_commission = Decimal('0.5')
         self.order_price = Decimal('10.0')
 
-    def test_utils_aggregation_orders_by_types(self):
+    def test_aggregation_orders_by_types(self):
         for i in range(0, 6):
             OrderFactory.create(
                 account=self.account,
@@ -34,7 +34,7 @@ class TestOrderCreation(TestCase):
             }, payload
         )
 
-    def test_utils_aggregation_orders_with_not_matched_orders(self):
+    def test_aggregation_orders_with_not_matched_orders(self):
         for i in range(0, 7):
             OrderFactory.create(
                 account=self.account,
@@ -54,7 +54,7 @@ class TestOrderCreation(TestCase):
             }, payload
         )
 
-    def test_utils_get_amount_from_avg_order_for_type_sell(self):
+    def test_get_amount_from_avg_order_for_type_sell(self):
         for i in range(0, 7):
             OrderFactory.create(
                 account=self.account,
@@ -67,24 +67,55 @@ class TestOrderCreation(TestCase):
         amount = get_amount_from_avg_order(Order.objects.all(), ORDER_TYPE_SELL, self.order_quantity)
         self.assertEqual(amount, (self.order_price * self.order_quantity) - self.order_commission)
 
-    def test_utils_get_amount_from_avg_order_for_type_buy(self):
+    def test_get_amount_from_avg_order_for_type_buy(self):
         for i in range(0, 2):
             OrderFactory.create(
+                type=ORDER_TYPE_SELL,
+
                 account=self.account,
                 pair='BTC-MANA',
-                type=ORDER_TYPE_SELL,
                 quantity=self.order_quantity,
                 commission=self.order_commission,
                 price=self.order_price,
             )
         for i in range(0, 4):
             OrderFactory.create(
+                type=ORDER_TYPE_BUY,
+
                 account=self.account,
                 pair='BTC-MANA',
-                type=ORDER_TYPE_BUY,
                 quantity=self.order_quantity,
                 commission=self.order_commission,
                 price=self.order_price,
             )
         amount = get_amount_from_avg_order(Order.objects.all(), ORDER_TYPE_SELL, self.order_quantity)
         self.assertEqual(amount, (self.order_price * self.order_quantity) - self.order_commission)
+
+    def test_get_not_matched_quantity(self):
+        for i in range(0, 4):
+            OrderFactory.create(
+                type=ORDER_TYPE_BUY,
+
+                account=self.account,
+                pair='BTC-MANA',
+                quantity=self.order_quantity,
+                commission=self.order_commission,
+                price=self.order_price,
+            )
+        for i in range(0, 1):
+            OrderFactory.create(
+                type=ORDER_TYPE_SELL,
+
+                account=self.account,
+                pair='BTC-MANA',
+                quantity=self.order_quantity,
+                commission=self.order_commission,
+                price=self.order_price,
+            )
+
+        payload = get_not_matched_quantity(Order.objects.all())
+        expected_not_matched_amount = Decimal(self.order_quantity * 3)
+
+        self.assertIsInstance(payload, tuple)
+        self.assertEqual(len(payload), 2)
+        self.assertEqual(payload, (ORDER_TYPE_BUY, expected_not_matched_amount))
