bb133e5501af39569a9790c1d9a50d0718d086c3
kuzentio <0585ec@gmail.com>
2018-06-04 13:17:49 +0300

    Add USD field for orders

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -6,9 +6,13 @@ from apps.order.models import Order
 class OrderSerializer(serializers.ModelSerializer):
     closed_at = serializers.DateTimeField(format='%d.%m.%Y %I:%M:%S %p')
     exchange = serializers.SerializerMethodField()
+    usd_price = serializers.SerializerMethodField()
 
     def get_exchange(self, obj):
         return obj.exchange.name
+
+    def get_usd_price(self, obj):
+        return str(round(obj.usd_price, 2))
 
     class Meta:
         model = Order
--- apps/api/views.py
+++ apps/api/views.py
@@ -1,16 +1,21 @@
 from datetime import datetime
 
+from django.db.models import Subquery, OuterRef
 from rest_framework import generics
 
 from apps.api.serializers import OrderSerializer
 from apps.order import utils
 from apps.order import constance
 from apps.order.models import Order
+from apps.rates.models import BTCRate
 
 
 class APIOrderView(generics.ListAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    usd_subquery = Subquery(
+        BTCRate.objects.filter(datetime__lte=OuterRef('closed_at')).order_by('-datetime').values('close')[:1]
+    )
 
     def get_queryset(self):
         extra_query = {}
@@ -39,7 +44,7 @@ class APIOrderView(generics.ListAPIView):
         if exchange is not None:
             extra_query['exchange__name'] = exchange
 
-        return qs.filter(**extra_query)
+        return qs.filter(**extra_query).annotate(usd_price=self.usd_subquery)
 
     def get_aggregations(self):
         aggregations = utils.aggregate_orders_by_types(
