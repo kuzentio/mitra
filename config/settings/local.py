a5781da764aaf6cda55c4137d90a29c8f67b1da1
kuzentio <0585ec@gmail.com>
2018-06-20 02:08:28 +0300

    add npm dependencies, and remove extras from repo

--- config/settings/local.py
+++ config/settings/local.py
@@ -19,28 +19,14 @@ MIDDLEWARE += [
 INTERNAL_IPS = ['localhost', '127.0.0.1']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '..', 'static'),
+STATICFILES_DIRS += [
+    os.path.join(BASE_DIR, '..', 'node_modules'),
 ]
-STATIC_URL = '/static/'
 
 INSTALLED_APPS += (
     'django_extensions',
     'debug_toolbar',
 )
-
-REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
-        'rest_framework_datatables.renderers.DatatablesRenderer',
-    ),
-    'DEFAULT_FILTER_BACKENDS': (
-        'rest_framework_datatables.filters.DatatablesFilterBackend',
-    ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework_datatables.pagination.DatatablesPageNumberPagination',
-    'PAGE_SIZE': 50,
-}
 
 SHELL_PLUS_POST_IMPORTS = (
     ('apps.order.factories', '*'),
