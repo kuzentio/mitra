a826b15529ffec6e0adad18e77214f571ca9de71
kuzentio <0585ec@gmail.com>
2018-08-20 17:17:23 +0300

    remove rates application

--- apps/rates/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from apps.rates.models import BTCRate
-
-
-@admin.register(BTCRate)
-class BTCRaresAdmin(admin.ModelAdmin):
-    list_display = ['id', 'close', 'volume', 'datetime']
