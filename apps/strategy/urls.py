85c51825679a8d19cce6ad60385a78c2711ed24f
kuzentio <0585ec@gmail.com>
2018-12-02 17:21:03 +0200

    fix url regexp

--- apps/strategy/urls.py
+++ apps/strategy/urls.py
@@ -7,5 +7,5 @@ app_name = 'strategy'
 
 urlpatterns = [
     url(regex=r'^$', view=login_required(StrategyView.as_view()), name='list'),
-    url(regex=r'^(?P<pk>[\d\w-]+)/$', view=login_required(StrategyDetailView.as_view()), name='detail'),
+    url(regex=r'^(?P<pk>[\d+])/$', view=login_required(StrategyDetailView.as_view()), name='detail'),
 ]
